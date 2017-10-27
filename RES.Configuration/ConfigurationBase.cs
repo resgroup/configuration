1481d79aaed67cc28acedfadb485811fc2d5581a
Cedd Burge <cedd.burge@res-ltd.com>
2017-10-27 19:23:09 +0100

    Fix up IsAvailable

--- RES.Configuration/ConfigurationBase.cs
+++ RES.Configuration/ConfigurationBase.cs
@@ -26,7 +26,7 @@ namespace RES.Configuration
         {
             Requires(setting != null);
 
-            return GetStringOrNull(setting) != null;
+            return !string.IsNullOrEmpty(GetStringOrNull(setting));
         }
 
         protected string GetString(string prefix, string setting)
