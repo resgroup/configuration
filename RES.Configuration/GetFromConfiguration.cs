ba5c668544449a71fca698b90ade8f6c98740890
Cedd Burge <cedd.burge@res-group.com>
2018-12-19 10:51:04 +0000

    Add support for Microsoft.Extenstions.Configuration (.net core)

---
+++ RES.Configuration/GetFromConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System.Configuration;
+using static System.Diagnostics.Contracts.Contract;
+using Microsoft.Extensions.Configuration;
+
+namespace RES.Configuration
+{
+    public class GetFromConfiguration : IConfigurationGetter
+    {
+        readonly IConfiguration configuration;
+
+        public GetFromConfiguration(IConfiguration configuration)
+        {
+            Requires(configuration != null);
+
+            this.configuration = configuration;
+        }
+        public string Get(string setting)
+        {
+            Requires(setting != null);
+
+            return configuration[setting];
+        }
+    }
+}
